e4b8083eb4f7b67e1ac2525ab7bd070a19012186
Gianni Maratta <marattagian15@gmail.com>
2022-08-17 17:40:58 -0300

    task type and mutation created

--- apollo/typeDefs.js
+++ apollo/typeDefs.js
@@ -1,8 +1,19 @@
 const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
+  type Task {
+    id: ID
+    title: String
+    description: String
+  }
+
   type Query {
     hello: String
+    getAllTasks: [Task]
+  }
+
+  type Mutation {
+    createTask(title: String, description: String): Task
   }
 `
 
